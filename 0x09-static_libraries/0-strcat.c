1bca4761cd3d8724b1bb2b1cb23f51ae9aa0973e
loocieomondi <loocieomondi@gmail.com>
2023-01-09 17:42:48 +0000

    p

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * *_strcat - concatenates @src to @dest
- * @src: the source string to append to @dest
- * @dest: the destiation string to be concatenated upon
- * Return: char
- */
 
 char *_strcat(char *dest, char *src)
 {
@@ -13,6 +7,13 @@ char *_strcat(char *dest, char *src)
 	int dest_len = 0;
 
 	while(dest[index++])
+	{
 		dest_len++;
-	for(index = 0; src[index]; index++)
+	}
+	for (index = 0; src[index]; index++)
+	{
 		dest[dest_len++] = src[index];
+
+	}
+	return(0);
+}
